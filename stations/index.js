c2b27c8af10285c7695eb20030694f6e13c0e069
Artur Iatsko <arturyatsko@gmail.com>
2022-07-24 14:05:18 -0400

    feat(stations): add no body error handling

--- stations/index.js
+++ stations/index.js
@@ -1,15 +1,24 @@
 module.exports = async function (context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
-  context.log(req.body);
-  const name = req.query.name || (req.body && req.body.name);
-  const responseMessage = name
-    ? "Hello, " +
-      name +
-      ". This HTT P triggered function executed successfully."
-    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  //   context.log("JavaScript HTTP trigger function processed a request.");
+  //   context.log(req.body);
+  //   const name = req.query.name || (req.body && req.body.name);
+  //   const responseMessage = name
+  //     ? "Hello, " +
+  //       name +
+  //       ". This HTT P triggered function executed successfully."
+  //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: responseMessage,
-  };
+  //   context.res = {
+  //     // status: 200, /* Defaults to 200 */
+  //     body: responseMessage,
+  //   };
+
+  try {
+    if (!req.body) throw new Error("no request body found");
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: error.message,
+    };
+  }
 };
