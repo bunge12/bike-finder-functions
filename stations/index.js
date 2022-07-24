eb093c6665f1b9730ebd83ed114281fc745a3eab
Artur Iatsko <arturyatsko@gmail.com>
2022-07-24 14:12:15 -0400

    feat(stations): download json data with axios

--- stations/index.js
+++ stations/index.js
@@ -1,20 +1,18 @@
+const { default: axios } = require("axios");
+
 module.exports = async function (context, req) {
-  //   context.log("JavaScript HTTP trigger function processed a request.");
-  //   context.log(req.body);
-  //   const name = req.query.name || (req.body && req.body.name);
-  //   const responseMessage = name
-  //     ? "Hello, " +
-  //       name +
-  //       ". This HTT P triggered function executed successfully."
-  //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-  //   context.res = {
-  //     // status: 200, /* Defaults to 200 */
-  //     body: responseMessage,
-  //   };
-
   try {
     if (!req.body) throw new Error("no request body found");
+    const status = (
+      await axios.get(
+        "https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_status"
+      )
+    ).data.data.stations;
+    const stations = (
+      await axios.get(
+        "https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_information"
+      )
+    ).data.data.stations;
   } catch (error) {
     context.res = {
       status: 500,
